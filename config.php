8a5a26ef30146172aeb43233f2ced8b62ef7d45c
Gade <68150467+G-aad-a@users.noreply.github.com>
2022-08-21 21:04:42 +0200

    del webhooks

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 return (object) array(
     'Key_Start_Name' => "GADE-", // EXAMPLE GADE-AJDIWOAWDJWDOIA 
-    'NotAllowed_Web' => "https://canary.discord.com/api/webhooks/925754955921494106/i3rrdLEFR7S-ZGjOKNdoDPSdUBRGbPe5YOYfMY0uRtqkuN1VvVvYWbuINQrzQUYqoWho",
-    'Expired_Web' => "https://canary.discord.com/api/webhooks/925754955921494106/i3rrdLEFR7S-ZGjOKNdoDPSdUBRGbPe5YOYfMY0uRtqkuN1VvVvYWbuINQrzQUYqoWho",
-    "WrongKey_Web" => "https://canary.discord.com/api/webhooks/925754955921494106/i3rrdLEFR7S-ZGjOKNdoDPSdUBRGbPe5YOYfMY0uRtqkuN1VvVvYWbuINQrzQUYqoWho",
-    "Success_Web" => "https://canary.discord.com/api/webhooks/925754955921494106/i3rrdLEFR7S-ZGjOKNdoDPSdUBRGbPe5YOYfMY0uRtqkuN1VvVvYWbuINQrzQUYqoWho",
-    "Disabled_Web" => "https://canary.discord.com/api/webhooks/925754955921494106/i3rrdLEFR7S-ZGjOKNdoDPSdUBRGbPe5YOYfMY0uRtqkuN1VvVvYWbuINQrzQUYqoWho",
+    'NotAllowed_Web' => "https://canary.discord.com/api/webhooks/925754955921494106/i3rrdLEFR7S-ZGjOdKNdoDPSdUBRGbPe5YOYfMY0uRtqkuN1VvVvYWbuINQrzQUYqoWho",
+    'Expired_Web' => "https://canary.discord.com/api/webhooks/925754955921494106/i3rrdLEFR7S-ZGjOKNddodDPSdUBRGbPe5YOYfMY0uRtqkuN1VvVvYWbuINQrzQUYqoWho",
+    "WrongKey_Web" => "https://canary.discord.com/api/webhooks/925754955921494106/i3rrdLEFR7S-ZGjOKNddoDPSdUBRGbPe5YOYfMY0uRtqkuN1VvVvYWbuINQrzQUYqoWho",
+    "Success_Web" => "https://canary.discord.com/api/webhooks/925754955921494106/i3rrdLEFR7S-d",
+    "Disabled_Web" => "https://canary.discord.com/api/webhooks/925754955921494106/i3rrdLEFR7S-ZGjOKdNdoDPSdUBRGbPe5YOYfMY0uRtqkuN1VvVvYWbuINQrzQUYqoWho",
 );
